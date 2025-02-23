3ac237484952f653c65073b70cd1b9d68311b973
Amit Parihar <pariharamit9676@gmail.com>
2025-02-23 21:25:31 +0530

    changes are complete

---
+++ backend/controllers/doctorController.js
@@ -0,0 +1,24 @@
+import doctorModel from "../models/doctorModel.js";
+
+const changeAvailability = async(req, res) => {
+
+    try{
+        const {docId} = req.body;
+        const docData = await doctorModel.findById(docId);
+
+        await doctorModel.findByIdAndUpdate(docId,{available:!docData.available})
+        res.json({
+            success: true,
+            message: "Doctor Availability Updated"
+        })
+    }
+    catch(error){
+        console.log(error);
+        return res.json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
+
+export {changeAvailability}
